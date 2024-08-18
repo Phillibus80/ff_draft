6f604f7b3e6e1349bb41bdeb6f237c0f0fb3d572
Phillibus <philmcelroy80@gmail.com>
2024-08-18 03:26:05 -0500

    refactor: player list styling.

--- src/components/player-list/player-list.jsx
+++ src/components/player-list/player-list.jsx
@@ -1,35 +1,45 @@
 import {getAllPlayers} from "@/api/players/playerStats";
+import styles from './player-list.module.scss';
 
 const PlayerList = async () => {
     const allPlayers = await getAllPlayers();
 
     const getTableHeaders = () => allPlayers[0] && Object.keys(allPlayers[0])
-        .filter(key => key !== 'id')
-        .map(key => <th key={key}>{key}</th>);
+        .filter(key => key === 'NAME' || key === 'POS' || key === 'FPTS')
+        .map(key => <th key={key}>{key === 'FPTS' ? 'Total Fantasy Points' : key}</th>);
 
     const getTableBody = () => {
         return allPlayers.length > 0 && allPlayers.map(player =>
             <tr key={`${player.NAME}_${player.YEAR}`}>{
-                Object.entries(player).map(([statKey, playerStat]) => {
-                    return statKey !== 'id'
-                        && <td key={`${player.NAME}_${player.YEAR}_${statKey}`}>
-                            {playerStat}
-                        </td>
-                })
-            }</tr>)
+                Object.entries(player)
+                    .filter(([key]) => key === 'NAME' || key === 'POS' || key === 'FPTS')
+                    .map(([statKey, playerStat]) => {
+                        return (
+                            <td
+                                className={statKey === 'FPTS' && styles.statNumber}
+                                key={`${player.NAME}_${player.YEAR}_${statKey}`}>
+                                {playerStat}
+                            </td>
+                        );
+                    })
+            }
+            </tr>
+        )
     };
 
     return (
-        <table>
-            <thead>
-            <tr>
-                {getTableHeaders()}
-            </tr>
-            </thead>
-            <tbody>
-            {getTableBody()}
-            </tbody>
-        </table>
+        <section className={styles.playerListTable}>
+            <table>
+                <thead>
+                <tr>
+                    {getTableHeaders()}
+                </tr>
+                </thead>
+                <tbody>
+                {getTableBody()}
+                </tbody>
+            </table>
+        </section>
     )
 }
 
