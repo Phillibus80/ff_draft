c80b808f0b1fd6eafce4d99dff831c410864da0e
Phillibus <philmcelroy80@gmail.com>
2024-08-25 23:30:51 -0500

    refactor: further removal of the socket

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 import { createServer } from 'node:http';
 import next from 'next';
-import { Server } from 'socket.io';
-import { socketConstants } from './app-constants.js';
-import { addMessage, getAllMessages } from './lib/chat/messages.js';
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
@@ -14,64 +11,7 @@ const handler = app.getRequestHandler();
 app.prepare().then(() => {
     const httpServer = createServer(handler);
 
-    const io = new Server(httpServer);
-
-    io.on(socketConstants.CONNECTION, (socket) => {
-        const currentMessages = getAllMessages();
-
-        // Join a specific room
-        socket.on(socketConstants.JOIN_ROOM, ({room}) => {
-            socket.join(room);
-
-            // Emit a welcome message to just the client in the room
-            socket.emit(socketConstants.WELCOME_MESSAGE, {
-                greeting: `Welcome User -> ${socket.id} to room:: ${room}`,
-                messages: currentMessages
-            });
-
-            // Broadcast to everyone in the room except the sender
-            socket.to(room).emit(socketConstants.NEW_USER_JOINED, {message: `${socket.id} has joined the room`});
-        });
-
-        // Handle a message sent to a specific room
-        // socket.on(socketConstants.SEND_ROOM_MESSAGE, ({room: {room}, message}) => {
-        //     io.to(room).emit(socketConstants.MESSAGE, {messageToSend: `${socket.id}: ${message}`});
-        // });
-
-        // Leave a room
-        // socket.on(socketConstants.LEAVE_ROOM, ({room}) => {
-        //     socket.leave(room);
-        //     console.log(`User ${socket.id} left room: ${room}`);
-        //     socket.to(room).emit(socketConstants.MESSAGE, `${socket.id} has left the room`);
-        // });
-
-        // Adding a new message
-        socket.on(socketConstants.ADD_MESSAGE, ({message, author}) => {
-            addMessage(message, author);
-            io.emit(socketConstants.NEW_MESSAGE_RECEIVED, {
-                addedMessage: {
-                    content: message,
-                    author
-                }
-            });
-        });
-
-        // Disconnecting
-        socket.on(socketConstants.DISCONNECT, () => {
-            console.log(`User disconnected: ${socket.id}`);
-        });
-
-        socket.on(socketConstants.END, () => {
-            socket.disconnect();
-        });
-    });
-
-    httpServer
-        .once(socketConstants.ERROR, (err) => {
-            console.error(err);
-            process.exit(1);
-        })
-        .listen(port, () => {
+    httpServer.listen(port, () => {
             console.log(`> Ready on http://${hostname}:${port}`);
         });
 });
