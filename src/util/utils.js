d226edd7ceab56995fb607e18768aee708664fb7
Phillibus <philmcelroy80@gmail.com>
2024-08-25 21:57:04 -0500

    refactor: remove Socket.io and replace with firebase's onValue function.

--- src/util/utils.js
+++ src/util/utils.js
@@ -11,14 +11,3 @@ export function debounce(func, wait) {
         timeout = setTimeout(later, wait);
     };
 }
-
-export const dedupMessages = (messages = [], messageToAdd = '') => {
-    if (!messageToAdd) return messages;
-
-    const currentMessages = [...messages, {content: messageToAdd}];
-    const messageContentArray = currentMessages.map(({content}) => content);
-
-    const duplicatesRemoved = [...new Set(messageContentArray)];
-
-    return duplicatesRemoved.map(txt => ({content: txt}));
-}
