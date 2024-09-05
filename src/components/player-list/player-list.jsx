56baf863e18e1389a4c451eac0b904177e621634
Phillibus <philmcelroy80@gmail.com>
2024-09-05 03:33:57 -0500

    refactor: clean up.

--- src/components/player-list/player-list.jsx
+++ src/components/player-list/player-list.jsx
@@ -1,12 +1,11 @@
 import {getAllPlayers} from "../../../lib/players/playerStats";
 import styles from './player-list.module.scss';
-import {PLAYER_KEYS} from "../../app-constants.js";
+import {PLAYER_KEYS, PREVIOUS_YEAR} from "@/app-constants.js";
 
 const PlayerList = async () => {
     const allPlayers = await getAllPlayers();
     const playerObjectArr = Object.values(allPlayers);
-    const date = new Date();
-    const lastYear = date.getFullYear() - 1;
+
     const isOneOfTheVisibleKeys = key => key === PLAYER_KEYS.NAME || key === PLAYER_KEYS.POSITION || key === PLAYER_KEYS.PTS;
 
     const getTableHeaders = (playerArr = []) => playerArr[0] && Object.keys(playerArr[0])
@@ -15,14 +14,14 @@ const PlayerList = async () => {
 
     const getTableBody = (playerArr = []) => {
         return playerArr.length > 0 && playerArr.map(player =>
-            <tr key={`${player.NAME}_${lastYear}`}>
+            <tr key={`${player.NAME}_${PREVIOUS_YEAR}`}>
                 {
                     Object.entries(player)
                         .filter(([key]) => key && isOneOfTheVisibleKeys(key))
                         .map(([statKey, playerStat]) =>
                             <td
                                 className={statKey === PLAYER_KEYS.PTS ? styles.statNumber : undefined}
-                                key={`${player.NAME}_${lastYear}_${statKey}`}>
+                                key={`${player.NAME}_${PREVIOUS_YEAR}_${statKey}`}>
                                 {playerStat}
                             </td>
                         )
