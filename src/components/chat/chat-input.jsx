e73f5f879f0162003261bcfbb0e3b071ac43017d
Phillibus <philmcelroy80@gmail.com>
2024-09-05 00:55:38 -0500

    refactor: author name based on the session.

--- src/components/chat/chat-input.jsx
+++ src/components/chat/chat-input.jsx
@@ -1,16 +1,18 @@
 "use client";
 
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 import {debounce} from "../../../lib/util/utils";
 import {addMessage} from "../../../lib/chat/messages.js";
 import {useSession} from "next-auth/react";
+import {useRouter} from "next/navigation.js";
+import {ROUTES} from "@/app-constants.js";
 
 const ChatInput = () => {
     const inputRef = useRef();
     const session = useSession();
-    console.log('Session:: ', session);
+    const router = useRouter();
 
-    const debouncedSubmit = debounce(async (message, author = 'Phillibus') => {
+    const debouncedSubmit = debounce(async (message, author) => {
         await addMessage(message, author);
     }, 300);
 
@@ -19,19 +21,34 @@ const ChatInput = () => {
         const message = inputRef.current.value;
 
         if (message) {
-            debouncedSubmit(message);
+            debouncedSubmit(message, session.data.user.username);
 
             // Reset the field
             inputRef.current.value = '';
         }
     };
 
-    return (
-        <form id='form' onSubmit={handleSubmit} action=''>
-            <input ref={inputRef} autoComplete='off'/>
-            <button>Send</button>
-        </form>
-    );
+    useEffect(() => {
+        if (session.status === 'unauthenticated') {
+            router.push(ROUTES.HOME);
+        }
+    }, [session.status, router]);
+
+    switch (session.status) {
+        case "loading":
+            return (
+                <div>Loading...</div>
+            );
+        case 'authenticated':
+            return (
+                <form id='form' onSubmit={handleSubmit} action=''>
+                    <input ref={inputRef} autoComplete='off'/>
+                    <button>Send</button>
+                </form>
+            );
+        default:
+            return null;
+    }
 };
 
 export default ChatInput;
