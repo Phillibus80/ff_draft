cc0c1083c8e46fb5d49fd33a9b9b729b06c87370
Phillibus <philmcelroy80@gmail.com>
2024-09-06 04:07:07 -0500

    refactor: pull players from the draft pool instead of the stats collection.

---
+++ src/components/player-list/player-list-utils.jsx
@@ -0,0 +1,31 @@
+import styles from './player-list.module.scss';
+import {PLAYER_KEYS, PREVIOUS_YEAR} from "@/app-constants.js";
+
+const isOneOfTheVisibleKeys = key => key === PLAYER_KEYS.NAME || key === PLAYER_KEYS.POSITION || key === PLAYER_KEYS.PTS;
+
+export const getTableHeaders = (playerArr = []) => playerArr[0] && Object.keys(playerArr[0])
+    .filter(key => key && isOneOfTheVisibleKeys(key))
+    .map(
+        key => <th key={key}>
+            {key === PLAYER_KEYS.PTS ? 'Total Fantasy Points' : key}
+        </th>
+    );
+
+export const getTableBody = (playerArr = []) => {
+    return playerArr.length > 0 && playerArr.map(player =>
+        <tr key={`${player.NAME}_${PREVIOUS_YEAR}`}>
+            {
+                Object.entries(player)
+                    .filter(([key]) => key && isOneOfTheVisibleKeys(key))
+                    .map(([statKey, playerStat]) =>
+                        <td
+                            className={statKey === PLAYER_KEYS.PTS ? styles.statNumber : undefined}
+                            key={`${player.NAME}_${PREVIOUS_YEAR}_${statKey}`}
+                        >
+                            {playerStat}
+                        </td>
+                    )
+            }
+        </tr>
+    )
+};
