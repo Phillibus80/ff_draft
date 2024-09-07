25da8290d4f4d2710332aaafce78acecbae464b6
Phillibus <philmcelroy80@gmail.com>
2024-09-07 05:09:16 -0500

    refactor: drafting players move to correct teams and are removed from the draft pool.

--- src/components/player-list/player-list.jsx
+++ src/components/player-list/player-list.jsx
@@ -5,17 +5,31 @@ import styles from './player-list.module.scss';
 import {useEffect, useState} from "react";
 import {getTableBody, getTableHeaders} from "@/components/player-list/player-list-utils.jsx";
 import {useSession} from "next-auth/react";
-import {draftPlayer} from "../../../lib/players/draftPlayer.js";
+import {draftPlayer as draftPlayerDB} from "../../../lib/players/draftPlayer.js";
 
 const PlayerList = () => {
     const {data: session, status} = useSession();
-    console.log("session", session);
     const [allPlayers, setAllPlayers] = useState({});
     const playerObjectArr = allPlayers && Object.values(allPlayers);
+
+    // TODO make this dynamic
+    const currentLeague = 'da_league';
+    const sessionLeague = !!session?.user?.leagues[currentLeague]
+        ? currentLeague
+        : '';
 
     useEffect(() => {
         getDraftPoolPlayers(setAllPlayers);
     }, []);
+
+    const draftPlayerWithSession = (leagueName, username, teamName) =>
+        player => draftPlayerDB(leagueName, username, teamName, player);
+
+    const draftPlayer = draftPlayerWithSession(
+        sessionLeague,
+        session?.user?.username,
+        session?.user?.team
+    );
 
     return (
         <section className={styles.playerListTable}>
