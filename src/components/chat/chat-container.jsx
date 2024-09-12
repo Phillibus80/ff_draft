699c74701c9167982f9fe6a77cae0f1c4c797f9a
Phillibus <philmcelroy80@gmail.com>
2024-09-12 02:59:47 -0500

    refactor: better layer the chat components with state and re-routing based on the session.

--- src/components/chat/chat-container.jsx
+++ src/components/chat/chat-container.jsx
@@ -6,6 +6,8 @@ import ChatWindow from "@/components/chat/chat-window.jsx";
 import ChatInput from "@/components/chat/chat-input.jsx";
 import {useSession} from "next-auth/react";
 import {getLeagueFromSession} from "../../../lib/util/utils.js";
+import {ROUTES} from "@/app-constants.js";
+import {useRouter} from "next/navigation.js";
 
 const ChatContainer = () => {
     const [currentMessages, setCurrentMessages] = useState([]);
@@ -14,21 +16,30 @@ const ChatContainer = () => {
     // Used for pagination
     const [lastKey, setLastKey] = useState('');
 
-    useEffect(() => {
-        if (status === 'authenticated') {
-            const sessionLeague = getLeagueFromSession(session, status);
+    const router = useRouter();
 
-            getAllMessages(sessionLeague, setCurrentMessages, lastKey, setLastKey);
+    useEffect(() => {
+        switch (status) {
+            case 'authenticated':
+                const sessionLeague = getLeagueFromSession(session, status);
+                getAllMessages(sessionLeague, setCurrentMessages, lastKey, setLastKey);
+
+                break;
+            case 'unauthenticated':
+                router.push(ROUTES.HOME);
+
+                break;
         }
 
     }, [status]);
 
-    return (
-        <>
-            <ChatWindow messages={currentMessages}/>
-            <ChatInput/>
-        </>
-    );
+    return status === 'loading'
+        ? <div>Loading...</div>
+        : (<>
+                <ChatWindow messages={currentMessages}/>
+                <ChatInput/>
+            </>
+        );
 }
 
 export default ChatContainer;
--- src/components/chat/chat-input.jsx
+++ src/components/chat/chat-input.jsx
@@ -1,17 +1,14 @@
 "use client";
 
-import {useEffect, useRef} from "react";
+import {useRef} from "react";
 import {debounce, getLeagueFromSession} from "../../../lib/util/utils";
 import {addMessage} from "../../../lib/chat/messages.js";
 import {useSession} from "next-auth/react";
-import {useRouter} from "next/navigation.js";
-import {ROUTES} from "@/app-constants.js";
 
 const ChatInput = () => {
     const inputRef = useRef();
     const {data: session, status} = useSession();
     const sessionLeague = getLeagueFromSession(session, status);
-    const router = useRouter();
 
     const debouncedSubmit = debounce(async (message, author, leagueName) => {
         await addMessage(message, author, leagueName);
@@ -29,27 +26,12 @@ const ChatInput = () => {
         }
     };
 
-    useEffect(() => {
-        if (status === 'unauthenticated') {
-            router.push(ROUTES.HOME);
-        }
-    }, [status, router]);
-
-    switch (status) {
-        case "loading":
-            return (
-                <div>Loading...</div>
-            );
-        case 'authenticated':
-            return (
-                <form id='form' onSubmit={handleSubmit} action=''>
-                    <input ref={inputRef} autoComplete='off'/>
-                    <button>Send</button>
-                </form>
-            );
-        default:
-            return null;
-    }
+    return (
+        <form id='form' onSubmit={handleSubmit} action=''>
+            <input ref={inputRef} autoComplete='off'/>
+            <button>Send</button>
+        </form>
+    );
 };
 
 export default ChatInput;
