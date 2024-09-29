6961e6e174ac3c6e71e1e60e6575df135ccb6e2a
Phillibus <philmcelroy80@gmail.com>
2024-09-29 03:23:16 -0500

    fix: 405 error after logging in

--- src/components/login-form/login-form.jsx
+++ src/components/login-form/login-form.jsx
@@ -3,14 +3,40 @@
 import {useFormState} from "react-dom";
 import styles from './login-form.module.scss';
 import {authenticate} from "@/app/actions/actions.js";
+import {useRef} from "react";
 
 const LoginForm = () => {
+    const userNameRef = useRef();
+    const passwordRef = useRef();
+
     const [, formAction] = useFormState(authenticate, {message: null});
 
     return (
-        <form action={formAction} className={styles.login_form}>
-            <input name='username' type="text" placeholder='Username'/>
-            <input name='password' type="password" placeholder='Password'/>
+        <form
+            action={() => {
+                console.log('Username:: ', userNameRef.current)
+                console.log('Password:: ', passwordRef.current)
+                authenticate(userNameRef.current, passwordRef.current)
+                    .then(res => console.log('Res:: ', res))
+            }}
+            className={styles.login_form}
+        >
+            <input
+                onChange={event => {
+                    userNameRef.current = event.target.value
+                }}
+                name='username'
+                type="text"
+                placeholder='Username'
+            />
+            <input
+                onChange={event => {
+                    passwordRef.current = event.target.value
+                }}
+                name='password'
+                type="password"
+                placeholder='Password'
+            />
 
             <input type='submit' value='Log in'/>
         </form>
