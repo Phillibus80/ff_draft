622430a655323a9b844d8df0a6035da3e5ef9cdd
Phillibus <philmcelroy80@gmail.com>
2024-10-02 04:31:55 -0500

    fix: context was not recognizing the rules slice and the drafted players was reading data from the wrong hook.

--- src/components/draft-tracker/draft-queue.jsx
+++ src/components/draft-tracker/draft-queue.jsx
@@ -1,4 +1,12 @@
+"use client";
+
+import {useContext} from "react";
+import {DraftContext} from "@/context/draft-room-context/draft-room-context.jsx";
+
 const DraftQueue = () => {
+    const {draftRules, rosterConstruction} = useContext(DraftContext);
+    console.log('Draft Rules:: ', draftRules);
+    console.log('Roster Construction:: ', rosterConstruction);
     return (
         <>Hello</>
     );
--- src/hooks/draft-tracker/draft-tracker-hooks.jsx
+++ src/hooks/draft-tracker/draft-tracker-hooks.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {SESSION_CONSTANTS} from "@/app-constants.js";
 import {getLeagueFromSession, stripStr} from "../../../lib/util/utils.js";
 import {getLeagueDraftUpdates} from "../../../lib/league/leagueDraft.js";
@@ -6,25 +6,21 @@ import {getAllRules} from "../../../lib/rules/rules.js";
 import {getRosterSlots} from "../../../lib/util/roster-utils.js";
 import {getDraftedPlayers} from "../../../lib/players/draftPlayer.js";
 
-export const useGetLeagueDraftDetails = (session, status) => {
-    const [currentDraftStatus, setCurrentDraftStatus] = useState(null);
-
+export const useGetLeagueDraftDetails = (session, status, setStateCallback) => {
     useEffect(() => {
         let unsubscribe;
 
         if (status === SESSION_CONSTANTS.AUTHENTICATED) {
             const leagueKey = getLeagueFromSession(session, status);
 
-            unsubscribe = getLeagueDraftUpdates(leagueKey, setCurrentDraftStatus);
+            unsubscribe = getLeagueDraftUpdates(leagueKey, setStateCallback);
         }
 
-        return () => unsubscribe && unsubscribe();
+        return () => !!unsubscribe ?? unsubscribe();
     }, [status]);
-
-    return currentDraftStatus || {};
 };
 
-export const useGetAllLeagueRules = (leagueName, session, status, setStateCallback) => {
+export const useGetCurrentDraftedRoster = (leagueName, session, status, setStateCallback) => {
     useEffect(() => {
         const leagueKey = stripStr(leagueName);
         let unsubscribe;
@@ -44,7 +40,30 @@ export const useGetAllLeagueRules = (leagueName, session, status, setStateCallba
                         unsubscribe = getDraftedPlayers(leagueKey, teamKey, rosterMap, setStateCallback);
                         setStateCallback(prevState => ({...rosterMap, ...prevState}));
                     } catch (e) {
-                        console.error('Issue getting the league rules.');
+                        console.error('Issue getting the drafted roster.');
+                        throw new Error('No roster returned.')
+                    }
+                })
+        }
+
+        // Clean up
+        return () => !!unsubscribe ?? unsubscribe();
+    }, [status]);
+}
+
+export const useGetLeagueRules = (leagueName, session, status, setStateCallback) => {
+    useEffect(() => {
+        const leagueKey = stripStr(leagueName);
+        let unsubscribe;
+
+        if (status === SESSION_CONSTANTS.AUTHENTICATED) {
+            getAllRules(leagueKey)
+                .then(res => {
+                    try {
+                        const {config} = res;
+                        setStateCallback(prev => ({...prev, ...config}));
+                    } catch (e) {
+                        console.error('Issue getting the rules.');
                         throw new Error('No rules returned.')
                     }
                 })
