d313db2abb9ee27ac81922813f92144c4ad30939
Phillibus <philmcelroy80@gmail.com>
2024-10-02 01:15:20 -0500

    refactor: create and integrate useGetAllLeagueRules hook

--- src/hooks/draft-tracker/draft-tracker-hooks.jsx
+++ src/hooks/draft-tracker/draft-tracker-hooks.jsx
@@ -1,7 +1,10 @@
 import {useEffect, useState} from "react";
 import {SESSION_CONSTANTS} from "@/app-constants.js";
-import {getLeagueFromSession} from "../../../lib/util/utils.js";
+import {getLeagueFromSession, stripStr} from "../../../lib/util/utils.js";
 import {getLeagueDraftUpdates} from "../../../lib/league/leagueDraft.js";
+import {getAllRules} from "../../../lib/rules/rules.js";
+import {getRosterSlots} from "../../../lib/util/roster-utils.js";
+import {getDraftedPlayers} from "../../../lib/players/draftPlayer.js";
 
 export const useGetLeagueDraftDetails = (session, status) => {
     const [currentDraftStatus, setCurrentDraftStatus] = useState(null);
@@ -19,4 +22,35 @@ export const useGetLeagueDraftDetails = (session, status) => {
     }, [status]);
 
     return currentDraftStatus || {};
+};
+
+export const useGetAllLeagueRules = (leagueName, session, status, setStateCallback) => {
+    useEffect(() => {
+        const leagueKey = stripStr(leagueName);
+        let unsubscribe;
+
+        if (status === SESSION_CONSTANTS.AUTHENTICATED) {
+            // Build the roster template based on the
+            // rules set
+            getAllRules(leagueKey)
+                .then(res => {
+                    try {
+                        const {config: {roster_construction}} = res;
+
+                        const rosterMap = getRosterSlots(roster_construction);
+
+                        // Populate the roster with drafted players
+                        const teamKey = stripStr(session?.user?.team);
+                        unsubscribe = getDraftedPlayers(leagueKey, teamKey, rosterMap, setStateCallback);
+                        setStateCallback(prevState => ({...rosterMap, ...prevState}));
+                    } catch (e) {
+                        console.error('Issue getting the league rules.');
+                        throw new Error('No rules returned.')
+                    }
+                })
+        }
+
+        // Clean up
+        return () => unsubscribe ? unsubscribe() : null;
+    }, [status]);
 }
