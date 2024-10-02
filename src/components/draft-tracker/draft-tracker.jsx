01f5ec66f82b11b1d705cd318f3fe474d636c3cf
Phillibus <philmcelroy80@gmail.com>
2024-10-02 05:40:09 -0500

    refactor: add team draft card component

--- src/components/draft-tracker/draft-tracker.jsx
+++ src/components/draft-tracker/draft-tracker.jsx
@@ -4,10 +4,11 @@ import Timer from "@/components/draft-tracker/timer.jsx";
 import {useContext} from "react";
 import {DraftContext} from "@/context/draft-room-context/draft-room-context.jsx";
 import DraftQueue from "@/components/draft-tracker/draft-queue.jsx";
+import * as styles from './draft-tracker.module.scss';
 
 const DraftTracker = () => {
-    const draftContext = useContext(DraftContext);
-    if (!draftContext?.leagueDraft?.COMMISSIONER) return;
+    const {leagueDraft, draftRules, rosterConstruction} = useContext(DraftContext);
+    if (!leagueDraft || !draftRules || !rosterConstruction) return;
 
     const {
         COMMISSIONER: commish,
@@ -15,15 +16,18 @@ const DraftTracker = () => {
         DRAFT_ORDER: draftOrder,
         TIMESTAMP_OF_LAST_SELECTION: timestampOfSelected,
         TIME_PER_SELECTION: timePerPick
-    } = draftContext;
+    } = leagueDraft;
 
-    return (<>
+    return (<section className={styles.draft}>
         <Timer
             currentTime={timestampOfSelected}
             timeAllowed={timePerPick}
         />
-        <DraftQueue/>
-    </>);
+        <DraftQueue
+            currentPosition={currentPos}
+            draftOrder={draftOrder}
+        />
+    </section>);
 };
 
 export default DraftTracker;
