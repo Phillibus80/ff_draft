2c194a6440ee3d3263d9daa4684311b6967c16c5
Phillibus <philmcelroy80@gmail.com>
2024-10-05 02:55:02 -0500

    refactor: Team Draft Card now gets all the data for the manager and some very basic styling

--- src/components/draft-tracker/team-draft-card.jsx
+++ src/components/draft-tracker/team-draft-card.jsx
@@ -3,11 +3,19 @@
 import * as styles from './draft-tracker.module.scss';
 
 const TeamDraftCard = ({teamName, teamMoto, draftPosition}) => {
+    if (!teamName || !teamMoto) return null;
+
+    const NUM_CHAR_SHOWN = 55;
+
+    const modifyTeamMoto = moto => moto.split("").length > NUM_CHAR_SHOWN
+        ? moto.split('').slice(0, NUM_CHAR_SHOWN).join('') + '...'
+        : moto;
+
     return (
         <div className={styles.teamDraftCard}>
-            <p>{teamName}</p>
-            <p>{teamMoto}</p>
-            <p>{draftPosition}</p>
+            <h3 className={styles.teamDraftCard_name}>{teamName}</h3>
+            <p className={styles.teamDraftCard_moto}>{modifyTeamMoto(teamMoto)}</p>
+            <p className={styles.teamDraftCard_pos}>{draftPosition}</p>
         </div>
     );
 }
--- src/components/player-list/player-list-utils.jsx
+++ src/components/player-list/player-list-utils.jsx
@@ -1,8 +1,18 @@
+/**
+ * @typedef {import('../../../lib/jsdoc/types.js').draftPlayerConfig} draftPlayerConfig
+ */
+
 import styles from './player-list.module.scss';
 import {PLAYER_KEYS, PREVIOUS_YEAR} from "@/app-constants.js";
 
 const isOneOfTheVisibleKeys = key => key === PLAYER_KEYS.NAME || key === PLAYER_KEYS.POSITION || key === PLAYER_KEYS.PTS;
 
+/**
+ * A helper function to generate jsx table header elements.
+ *
+ * @param {Array<draftPlayerConfig>} playerArr - the array of players in the draft pool
+ * @return {*|Array<JSX.Element>} - the th elements
+ */
 export const getTableHeaders = (playerArr = []) => playerArr[0] && Object.keys(playerArr[0])
     .filter(key => key && isOneOfTheVisibleKeys(key))
     .map(
@@ -13,6 +23,14 @@ export const getTableHeaders = (playerArr = []) => playerArr[0] && Object.keys(p
         </th>
     );
 
+/**
+ * A helper function to generate jsx table row and table data elements.
+ *
+ * @param {Array<draftPlayerConfig>} playerArr - the array of players in the draft pool
+ * @param {function} draftPlayerCallback - the setState callback that sets the drafted player
+ * @param {string} username - the username key
+ * @return {false|Array<JSX.Element>} - the tr and td elements
+ */
 export const getTableBody = (playerArr = [], draftPlayerCallback, username) => {
     return playerArr.length > 0
         && playerArr.map(
