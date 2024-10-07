b7898548547c9dece2b21429d3416201075d46c3
Phillibus <philmcelroy80@gmail.com>
2024-10-07 02:32:21 -0500

    refactor: some parts of the chat container were showing when session is unauthenticated, also added the useGetAllMessages hook.

--- src/components/chat/chat-container.jsx
+++ src/components/chat/chat-container.jsx
@@ -1,13 +1,12 @@
 "use client";
 
-import {useEffect, useState} from "react";
-import {getAllMessages} from "../../../lib/chat/messages.js";
+import {useState} from "react";
 import ChatWindow from "@/components/chat/chat-window.jsx";
 import ChatInput from "@/components/chat/chat-input.jsx";
 import {useSession} from "next-auth/react";
-import {getLeagueFromSession} from "../../../lib/util/utils.js";
 import {SESSION_CONSTANTS} from "@/app-constants.js";
 import * as styles from './messages.module.scss';
+import {useGetAllMessages} from "@/hooks/draft-tracker/chat-hooks.jsx";
 
 const ChatContainer = () => {
     const [currentMessages, setCurrentMessages] = useState([]);
@@ -16,31 +15,16 @@ const ChatContainer = () => {
     // Used for pagination
     const [lastKey, setLastKey] = useState('');
 
-    useEffect(() => {
-        let unsubscribe;
+    useGetAllMessages(session, status, setCurrentMessages, lastKey, setLastKey);
 
-        switch (status) {
-            case SESSION_CONSTANTS.AUTHENTICATED:
-                const sessionLeague = getLeagueFromSession(session, status);
-                unsubscribe = getAllMessages(sessionLeague, setCurrentMessages, lastKey, setLastKey);
-
-                break;
-        }
-
-        return () => unsubscribe && unsubscribe();
-
-    }, [status]);
-
-    return status === SESSION_CONSTANTS.LOADING
-        ? <div>Loading...</div>
-        : (
-            <section className={styles.chat_container}>
-                <ChatWindow messages={currentMessages}/>
-                <div className={styles.chat_input}>
-                    <ChatInput/>
-                </div>
-            </section>
-        );
+    return status === SESSION_CONSTANTS.AUTHENTICATED ?? (
+        <section className={styles.chat_container}>
+            <ChatWindow messages={currentMessages}/>
+            <div className={styles.chat_input}>
+                <ChatInput/>
+            </div>
+        </section>
+    );
 }
 
 export default ChatContainer;
